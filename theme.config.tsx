45b134c12f75e6c8023eb1f4f36c294be1e5aaa4
Oein <oein0219@gmail.com>
2023-01-10 21:05:33 +0900

    change info

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Oein과 함께하는 TS 강좌</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Oein/typescript-lecture",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/Oein/typescript-lecture",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Oein과 함께하는 Typescript 강좌",
   },
-}
+};
 
-export default config
+export default config;
